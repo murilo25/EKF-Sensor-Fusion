5d89d1df5c99c9f95c1ba98dd4be6ca9d3f573df
Murilo Pinheiro <muriloaugusto25@hotmail.com>
2020-07-05 11:38:08 -0500

    Add kalman filter equations

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,16 +26,56 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+    x_ = F_ * x_;
+    MatrixXd Ft = F_.transpose();
+    P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+    VectorXd z_pred = H_ * x_;
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+	
+    VectorXd z_pred;
+	VectorXd y = z - z_pred;
+	MatrixXd H_j;
+	
+	float Px2 = x_[0]*x_[0];
+	float Py2 = x_[1]*x_[1];
+	z_pred[0] = sqrt(Px2 + Py2);
+	z_pred[1] = atan2(x_[1]/x_[0]);
+	z_pred[2] = (x_[0]*x_[2] + x_[1]*x_[3])/(sqrt(Px2 + Py2));
+	
+	H_j = tools.CalculateJacobian(x_)
+	
+    MatrixXd Ht = H_j.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
