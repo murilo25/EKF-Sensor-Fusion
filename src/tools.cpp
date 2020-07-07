cc81c68620303d55acd87d0e6acbb7c545a0a013
Murilo Pinheiro <muriloaugusto25@hotmail.com>
2020-07-07 18:13:32 -0500

    Fix calculateRMSE() syntax

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,14 +15,14 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd>& estimations, const vector<
     rmse << 0, 0, 0, 0;
 
     if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
-        cout << "Invalid estimation or ground_truth data" << endl;
+        std::cout << "Invalid estimation or ground_truth data" << std::endl;
         return rmse;
     }
 
     for (int i = 0; i < estimations.size(); i++)
     {
         VectorXd diff = estimations[i] - ground_truth[i];
-        rmse+=diff.array()* diff.array();   //term by term multiplication
+        rmse += diff.array() * diff.array();   //term by term multiplication
     }
     rmse = rmse / estimations.size();
     rmse = rmse.array().sqrt();
